8e9d3764b6269404f022a9d313af7f295f2180a7
Luis Velito <domiyi@luis.local>
2014-05-22 04:35:54 -0500

    comienzo del trabajo

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  	<head>
+	    <meta charset="utf-8">
+	    <title>Prueba App</title>
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="assets/css/nomalize.css" />
+		<link rel="stylesheet" type="text/css" href="assets/fonts/fonts.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+	    <!--[if lt IE 9]>
+	      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	    <![endif]-->
+		<script type="text/javascript" src="assets/js/jquery-1.8.2.js"></script>	
+		<!-- Add fancyBox -->
+		<link rel="stylesheet" href="assets/fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+		<script type="text/javascript" src="assets/fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
+		<!-- Optionally add helpers - button, thumbnail and/or media -->
+		<link rel="stylesheet" href="assets/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
+		<script type="text/javascript" src="assets/fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.6"></script>
+		<script type="text/javascript" src="assets/js/funciones.js"></script>	
+	</head>
+	<body>
+		<div id="container">
+			
+		</div>
+	</body>
+</html>
